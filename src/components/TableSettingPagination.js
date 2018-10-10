a683d0b57fa7ab70f2034762f5ffdd9e708d4e6b
hasanein <hasaninqh@yahoo.com>
2018-10-10 15:11:33 +0100

    new changes by hasanein most of the setting up table has been done

---
+++ src/components/TableSettingPagination.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import data from '../data/tableSetting.json';
+const TableSettingPagination = (props)=>{
+    
+      return data.map((item,index) =>
+          <div>
+              <button onClick={()=>props.setCurrentStep(index)} value={item.id}>{item.id}</button>
+          </div>
+       )
+    
+}
+export default TableSettingPagination
--- src/components/TableSetup.js
+++ src/components/TableSetup.js
@@ -1,7 +1,41 @@
-import React from 'react';
-const TableSetup = () => (
-  <div>
-    <h2>Welcome to Table setup</h2>
-  </div>
-);
+import React, { Component } from 'react';
+import data from '../data/tableSetting.json';
+import TableSettingPagination from './TableSettingPagination.js'
+class  TableSetup extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      currentStep:1
+    }
+  }
+   setCurrentStep=(index)=>{
+     console.log(index);
+    //let n = this.state.currentStep;
+    this.setState({
+      currentStep:index
+ 
+   })
+        
+   }
+  
+  render() {
+    console.log(data);
+    const filteredItem = data.filter((item)=> item.id === this.state.currentStep)
+    console.log(filteredItem);
+    return (
+       <div>
+        <h2>Welcome to Table setup</h2>
+        <TableSettingPagination setCurrentStep={this.setCurrentStep}/>  
+        
+              <div>
+                 <img src={filteredItem[0].url} alt={filteredItem[0].title}/>
+                 <h1>{filteredItem[0].title}</h1>
+             </div>
+       
+       </div>
+    );
+  }
+}
+ 
+
 export default TableSetup;
