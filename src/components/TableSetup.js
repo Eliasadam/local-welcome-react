ea0c8f75cf932e1df026c1fcd573fb7409f441c7
raj <rraju12@gmail.com>
2018-10-10 15:18:11 +0100

    Add missing files

--- src/components/TableSetup.js
+++ src/components/TableSetup.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import logo from '../images/logo.svg';
+console.log('logo', logo);
+const btnHandle = e => {
+  console.log('event', e.target.value);
+};
 const TableSetup = () => (
   <div>
     <h2>Welcome to Table setup</h2>
+    <img src={logo} alt="No img" />
+    <input type="button" onClick={btnHandle} value="2" />
   </div>
 );
 export default TableSetup;
