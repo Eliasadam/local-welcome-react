f896527efc4579d6508c13294d8dc3fef49b4a26
raj <rraju12@gmail.com>
2018-10-16 19:17:12 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/local-welcome-react/precache-manifest.e19a5de9ee79ee666a44726ef28379d6.js"
+  "/local-welcome-react/precache-manifest.1124f24df2aba3f87e9f076a89e7f0cb.js"
 );
 
 workbox.clientsClaim();
