2123cb5281bb242b5e85bec0819e6eb681e59467
hasanein <hasaninqh@yahoo.com>
2018-10-17 23:01:10 +0100

    WithFormik updated

--- src/App.js
+++ src/App.js
@@ -1,56 +1,53 @@
 import React, { Component } from 'react';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
+//import { Formik } from 'formik';
+//import * as Yup from 'yup';
 import DashboardPage from './components/pages/DashboardPage';
 import FormikEnhancer from './components/FormikEnhancer';
 import users from './data/login.json';
 import './App.css';
 
 class App extends Component {
-  state = {
-    isLoggedIn: false,
-    userValue: '',
-    passwordValue: '',
-    errorMessage: ''
-  };
-  handleUserChange = e => {
-    this.setState({
-      userValue: e.target.value,
-    });
-  };
-  handlePasswordChange = e => {
-    this.setState({
-      passwordValue: e.target.value,
-    });
-  };
-  loginMe = e => {
-    e.preventDefault();
+//   state = {
+//     isLoggedIn: false,
+//     userValue: '',
+//     passwordValue: '',
+//     errorMessage: ''
+//   };
+//   handleUserChange = e => {
+//     this.setState({
+//       userValue: e.target.value,
+//     });
+//   };
+//   handlePasswordChange = e => {
+//     this.setState({
+//       passwordValue: e.target.value,
+//     });
+//   };
+//   loginMe = e => {
+//     e.preventDefault();
 
-    const authorised = users.some(
-      ({ user, password }) =>
-        user === this.state.userValue && password === this.state.passwordValue
-    );
+//     const authorised = users.some(
+//       ({ user, password }) =>
+//         user === this.state.userValue && password === this.state.passwordValue
+//     );
 
-    this.setState({
-      isLoggedIn: authorised,
-      errorMessage: authorised ? null : 'Invalid username or password',
-    });
-  };
+//     this.setState({
+//       isLoggedIn: authorised,
+//       errorMessage: authorised ? null : 'Invalid username or password',
+//     });
+//   };
 
   render() {
     return (
      
       <div className="App">
      
-        {this.state.isLoggedIn ? (
-          <DashboardPage />
-        ) : (
-          
-          <FormikEnhancer loginMe={this.loginMe} handleUserChange={this.handleUserChange} handlePasswordChange={this.handlePasswordChange}/>
-        )}
+          <FormikEnhancer />
+        
       </div>
     );
   }
 }
+
 
 export default App;
--- src/components/FormikEnhancer.js
+++ src/components/FormikEnhancer.js
@@ -1,53 +1,105 @@
 import React from 'react';
 import { withFormik,Form,Field } from 'formik';
 import * as Yup from 'yup';
-//import DashboardPage from './components/pages/DashboardPage';
-//import users from './data/login.json';
-//import 'App.css';
+import DashboardPage from './pages/DashboardPage';
+import users from '../data/login.json';
+import '../App.css';
+class LoginForm extends React.Component{
+    state = {
+        isLoggedIn: false,
+        userValue: '',
+        passwordValue: '',
+        errorMessage: ''
+      };
+      handleUserChange = e => {
+        this.setState({
+          userValue: e.target.value,
+        });
+      };
+      handlePasswordChange = e => {
+        this.setState({
+          passwordValue: e.target.value,
+        });
+      };
+      loginMe = e => {
+        e.preventDefault();
+    
+        const authorised = users.some(
+          ({ user, password }) =>
+            user === this.state.userValue && password === this.state.passwordValue
+        );
+    
+        this.setState({
+          isLoggedIn: authorised,
+          errorMessage: authorised ? null : 'Invalid username or password',
+        });
+        console.log(authorised)
+      };
+    
+//
+//   values,
+//   errors,
+//   touched,
+//   dirty,
+//   isSubmitting,
+//   handleBlur,
+//   handleReset,
+//   handleUserChange,
+//   handlePasswordChange,
+//   loginMe
 
-const loginForm = ({
-  values,
-  errors,
-  touched,
-  dirty,
-  isSubmitting,
-  handleBlur,
-  handleReset,
-  handleUserChange,
-  handlePasswordChange,
-  loginMe
-})=>(
+render(){
+  
+    
+    return(
+
+   
+     this.state.isLoggedIn ? ( 
+     <DashboardPage />
+     ) : (
+         
+    <div className="formWrapper">     
+    <p>{this.state.errorMessage}</p>
   <Form>
     <div>
-      {touched.userName && errors.userName &&<p>{errors.userName}</p>}
-      <Field type ='text' name='userName' placeholder="User Name" onBlur={handleBlur}  className={
-                      errors.userName && touched.userName ? 'text-input error' : 'text-input'
+      {this.props.touched.userName && this.props.errors.userName &&  <div className="input-feedback">{this.props.errors.userName}</div>}
+      <Field type ='text' name='userName' placeholder="User Name" value={this.props.values.userName} onBlur={this.props.handleBlur} onChange={(e)=>{this.props.handleChange(e); this.handleUserChange(e)}}
+      className={this.props.errors.userName && this.props.touched.userName ? 'text-input error' : 'text-input'
                     }/>
     </div>
     <div>
-      {touched.password && errors.password &&<p>{errors.password}</p>}
-      <Field type ='password' name='password' placeholder="password" onBlur={handleBlur} className={
-                      errors.userName && touched.userName ? 'text-input error' : 'text-input'
+      {this.props.touched.password && this.props.errors.password &&  <div className="input-feedback">{this.props.errors.password}</div>}
+      <Field type ='password' name='password' placeholder="password" onBlur={this.props.handleBlur} onChange={(e)=>{this.props.handleChange(e); this.handlePasswordChange(e)}}
+      className={this.props.errors.password && this.props.touched.password ? 'text-input error' : 'text-input'
                     }/>
     </div>
       <button
         type="button"
         className="outline"
-        onClick={handleReset}
-        disabled={!dirty || isSubmitting}>
+        onClick={this.props.handleReset}
+        disabled={!this.props.dirty || this.props.isSubmitting}>
         Reset
       </button>
-      <button disabled={isSubmitting}>submit</button>
+      <button type='submit' disabled={this.props.isSubmitting}>submit</button>
     </Form>
-)
+    </div>
+  )
+ 
+  )
 
+  }
+ 
+}            
 const FormikEnhancer = withFormik({
-  mapPropsToValues({userName,password,loginMe}){
-   console.log(loginMe)
+  mapPropsToValues:(props)=>{
+
     return{
-      userName:userName || '',
-      password:password || '',
-      loginMe:loginMe
+      userName:props.userName || '',
+      password:props.password || '',
+      loginMe:props.loginMe
+    
+   
+      
     }
   },
   validationSchema: Yup.object().shape({
@@ -58,21 +110,20 @@ const FormikEnhancer = withFormik({
       .min(3, "your password should be longer")
       .required('required.'),
   }),
-  handleSubmit(values,{setErrors, setSubmitting,resetForm}){
+  handleSubmit(values,{setSubmitting, resetForm}){
      setTimeout(()=>{
-      console.log(values.loginMe)
+  values.loginMe()
+        resetForm()
+        setSubmitting(false);
      },2000)
-  }
-})(loginForm)
+ }
+})(LoginForm)
 
   
   
 
-//   <div>
-//     <h2>Welcome to Login page!</h2>
-//     <button type="submit">Log in</button>
-//   </div>
-// );
+
+
 export default FormikEnhancer;
 
 
